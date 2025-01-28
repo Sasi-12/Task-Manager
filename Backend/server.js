01519addc59b6b853c2cdb663be69fd94e3a5878
Sasi367 <sasikumari0604@gmail.com>
2025-01-28 20:00:45 +0530

    First commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,54 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const taskRoutes = require('./routes/taskRoutes');
+const db = require('./config/db');
 
 const app = express();
 const PORT = 5000;
 
 // Middleware
+app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-app.use(express.json());
 
 // MongoDB Connection
-mongoose
-  .connect('mongodb://127.0.0.1:27017/taskmanager', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.log(err));
+db();
 
-// Task Schema & Model
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  dueDate: { type: Date },
-  isCompleted: { type: Boolean, default: false },
-});
+// Routes
+app.use('/tasks', taskRoutes);
 
-const Task = mongoose.model('Task', taskSchema);
-
-// API Routes
-app.get('/tasks', async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
-});
-
-app.post('/tasks', async (req, res) => {
-  const newTask = new Task(req.body);
-  await newTask.save();
-  res.json(newTask);
-});
-
-app.put('/tasks/:id', async (req, res) => {
-  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(updatedTask);
-});
-
-app.delete('/tasks/:id', async (req, res) => {
-  await Task.findByIdAndDelete(req.params.id);
-  res.json({ message: 'Task deleted' });
-});
-
+// Start Server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
