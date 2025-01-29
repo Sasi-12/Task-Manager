f62c0db80791510c7fc3aeb50b1f60f3cec2d352
Sasi367 <sasikumari0604@gmail.com>
2025-01-29 14:31:45 +0530

    commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/taskmanager', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb://127.0.0.1:27017/taskmanager');
     console.log('Connected to MongoDB');
   } catch (err) {
-    console.error(err);
+    console.error('MongoDB connection error:', err);
     process.exit(1);
   }
 };
