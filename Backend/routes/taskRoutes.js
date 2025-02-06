cc5f3f4c5d1c2e552695081b345a31dc3e7ccdb9
Sasi367 <sasikumari0604@gmail.com>
2025-02-06 20:51:14 +0530

    commit

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -3,26 +3,22 @@ const Task = require('../models/Task');
 
 const router = express.Router();
 
-// Get all tasks
 router.get('/', async (req, res) => {
   const tasks = await Task.find();
   res.json(tasks);
 });
 
-// Create a new task
 router.post('/', async (req, res) => {
   const newTask = new Task(req.body);
   await newTask.save();
   res.json(newTask);
 });
 
-// Update a task
 router.put('/:id', async (req, res) => {
   const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
   res.json(updatedTask);
 });
 
-// Delete a task
 router.delete('/:id', async (req, res) => {
   await Task.findByIdAndDelete(req.params.id);
   res.json({ message: 'Task deleted' });
