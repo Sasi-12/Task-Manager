85503de137895a792331b9a4c9ffe176d8684a67
Sasi367 <sasikumari0604@gmail.com>
2025-02-14 20:43:27 +0530

    commit

--- Backend/models/Task.js
+++ Backend/models/Task.js
@@ -4,7 +4,7 @@ const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   dueDate: { type: Date },
-  isCompleted: { type: Boolean, default: false },
+  isComplete: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Task', taskSchema);
